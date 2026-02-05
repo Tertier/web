e7ac6ac94f43babd3fd0670c6e8d775fe4f6e080
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2026-02-05 17:50:51 +0100

    system color scheme matched

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,13 +3,25 @@
 import {HeroUIProvider} from "@heroui/system";
 import {useRouter} from "next/navigation";
 import {FunctionComponent, PropsWithChildren} from "react";
+import {ThemeProvider} from "next-themes";
 
 export default (({children}) => {
   const router = useRouter();
 
   return (
-    <HeroUIProvider navigate={router.push}>
-      {children}
+    <HeroUIProvider
+      navigate={router.push}
+    >
+      <ThemeProvider
+        attribute={["class", "data-theme"]}
+        defaultTheme="system"
+        enableColorScheme
+        enableSystem
+        disableTransitionOnChange
+        forcedTheme="system"
+      >
+        {children}
+      </ThemeProvider>
     </HeroUIProvider >
   );
 }) satisfies FunctionComponent<PropsWithChildren>;
